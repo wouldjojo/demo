8ea522159996eb3cbec9d62ac74c4defd23d5e2e
wouldjojo <307990341@qq.com>
2018-03-16 14:46:35 +0800

    Add files via upload

---
+++ MyTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ConsoleApp4
+{
+   [TestFixture]
+    class MyTest
+    {
+        [TestCase]
+        public void  add()
+        {
+            Mymath math = new Mymath();
+            Assert.AreEqual(3, math.Add(2,1),"Test add pass");
+
+        }
+            
+        [TestCase]
+        public void sub()
+        {
+            Mymath math = new Mymath();
+            Assert.AreEqual(1, math.sub(2, 1),"Test sub pass");
+        }
+    }
+}
---
+++ Mymath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+   public  class Mymath
+    {
+        public int Add(int a ,int b)
+        {
+            return a + b;
+        }
+
+        public int sub (int a,int b)
+        {
+            return a - b;
+        }
+    }
+}
